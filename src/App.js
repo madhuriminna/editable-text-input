16e176e0af97d508a5c2f583488f0724d24f8ed9
root <minnamadhuri@gmail.com>
2022-11-05 09:16:16 +0000

    editable text input message

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import {Component} from 'react'
+import './App.css'
+import {Container, Div, Div2, Head, Btn1, Para} from './StyledComponents'
+
+// Replace your code here
+class App extends Component {
+  state = {display1: true, inputType: ''}
+
+  getValue = event => {
+    this.setState({inputType: event.target.value})
+  }
+
+  getChange = () => {
+    this.setState(prevState => ({display1: !prevState.display}))
+  }
+
+  getSave = () => {
+    const {inputType} = this.state
+    console.log(inputType)
+    return (
+      <Div2>
+        <input
+          onChange={this.getValue}
+          placeholder="enter Text"
+          value={inputType}
+          type="text"
+        />
+        <Btn1 type="button" onClick={this.getChange}>
+          Save
+        </Btn1>
+      </Div2>
+    )
+  }
+
+  getEdit = () => {
+    const {inputType} = this.state
+    return (
+      <Div2>
+        <Para>{inputType}</Para>
+        <Btn1 type="button" onClick={this.getChange} className="btn1">
+          Edit
+        </Btn1>
+      </Div2>
+    )
+  }
+
+  render() {
+    const {display1} = this.state
+    return (
+      <Container>
+        <Div>
+          <Head>Editable Text Input</Head>
+          {display1 ? this.getSave() : this.getEdit()}
+        </Div>
+      </Container>
+    )
+  }
+}
+
+export default App
---
+++ src/StyledComponents.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: pink;
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  background-color: white;
+  border-radius: 0px;
+`
+
+export const Head = styled.h1`
+  color: black;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`
+export const Para = styled.p`
+  font-size: 18px;
+`
+export const Input1 = styled.input`
+  margin-right: 8px;
+  background-color: white;
+`
+export const Btn1 = styled.button`
+  background-color: blueviolet;
+  color: white;
+  border-radius: 5px;
+  border-width: 0px;
+`
+export const Div2 = styled.div`
+  display: flex;
+  flex-direction: row;
+`
